4f89565257f71af58892a729d73a3f849d4e8517
tonylin46391 <tonylin46391@gmail.com>
2025-12-03 11:22:39 +0800

    1001_Judy_RS_W14.py :01

--- 1001_Judy_RS_W14.py
+++ 1001_Judy_RS_W14.py
@@ -245,7 +245,7 @@ with col2:
 #    if st.button("▶ 例句（中）"):
 #        # 呼叫 gTTS 播放中文例句 (中文 'zh-tw')
 #        play_audio_gtts(sentence_zh, 'zh-tw')
-with col4: # 英文定義按鈕
+with col3: # 英文定義按鈕
     if st.button("▶ 定義（英）"):
         # 呼叫 gTTS 播放英文定義 (英文 'en')
         play_audio_gtts(definition, 'en')
